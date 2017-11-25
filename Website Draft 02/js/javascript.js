6661b74ecae34d0d2bbe32c045a6e1b7adace7ec
Pedro Pires <33130521+PaperButt@users.noreply.github.com>
2017-11-25 21:21:27 +0000

    Pretty Nav Bar Button
    Pretty :astonished:

--- Website Draft 02/js/javascript.js
+++ Website Draft 02/js/javascript.js
@@ -12,6 +12,7 @@ $(function() {
     console.log(1);
     switch (nav_is_shown) {
       case false:
+        $("#right_side").css("transform", "translateY(0) translateX(50px)");
         $("#darkness").css("opacity", "0.6");
         $("#darkness").css("pointer-events", "all");
         $("#nav_button1, #nav_button2, #nav_button3, #nav_button4").css("visibility", "visible");
@@ -51,6 +52,7 @@ $(function() {
           $("#nav_button1, #nav_button2, #nav_button3, #nav_button4").css("visibility", "hidden");
           $("#nav_ball svg").css("transform", "rotate(" + 0 + "deg)");
           $("#nav_bar").css("transform", "rotate(95deg)");
+          $("#right_side").css("transform", "translateY(100px) translateX(-500px)");
         }, 300);
         setTimeout(function() {
           console.log(2);
